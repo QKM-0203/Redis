bb95f546ea1ed8dcfb61807836895f5092cc345d
QKM-0203 <com_qkm@xiyoulinux.org>
2021-04-23 10:16:43 +0800

    redis+mybatis+mysql

--- Springboot-Redis/src/main/java/com/qkm/springbootredis/Config/RedisConfig.java
+++ Springboot-Redis/src/main/java/com/qkm/springbootredis/Config/RedisConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableCaching
 @Configuration
 @MapperScan({"com/qkm/springbootredis/Mapper"})
 public class RedisConfig {
